bf84727ce2a346787262ac6c4fc9cc2e8ca82550
iqmeta <github@iqmeta.com>
2016-02-16 10:36:23 +0100

    Update CDVClipboard.m

--- src/ios/CDVClipboard.m
+++ src/ios/CDVClipboard.m
@@ -2,6 +2,7 @@
 #import <Cordova/CDVPlugin.h>
 #import <Cordova/CDVPluginResult.h>
 #import "CDVClipboard.h"
+#import "NSDataBase64.h"
 
 @implementation CDVClipboard
 
@@ -24,6 +25,24 @@
 	    
 	    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:text];
 	    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+	}];
+}
+
+-(NSString *)UIImageToBaseSixtyFour:(UIImage *)sourceImage
+{
+        NSData *imageData = UIImageJPEGRepresentation(sourceImage, 1.0); 
+        NSString *base64 = [NSString stringWithFormat:@"%@",[UIImageJPEGRepresentation(sourceImage, 0.95) base64EncodedString]];
+        return base64;
+}
+
+- (void)pasteImage:(CDVInvokedUrlCommand*)command {
+	[self.commandDelegate runInBackground:^{
+		
+        UIImage *img = [UIPasteboard generalPasteboard].image;		        
+         NSString *text =[self UIImageToBaseSixtyFour:img];
+	    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:text];
+	    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+	    
 	}];
 }
 
